91040cfbbaaba8e7efd23d00b7d89819f57c6929
BookyAde <alabiwinner9@gmail.com>
2026-06-23 09:39:55 +0100

    fix: Address Ruff linting errors in migration file

--- backend/alembic/versions/45dbd516e93a_add_content_tables.py
+++ backend/alembic/versions/45dbd516e93a_add_content_tables.py
@@ -5,8 +5,8 @@ Revises: 6c4293f9ef1d
 Create Date: 2026-06-21 23:29:52.848190
 """
 from __future__ import annotations
+from collections.abc import Sequence
 from pgvector.sqlalchemy import Vector
-from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
@@ -14,9 +14,9 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '45dbd516e93a'
-down_revision: Union[str, None] = '6c4293f9ef1d'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = '6c4293f9ef1d'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
